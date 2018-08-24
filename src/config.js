209a1563fc11ebd5672ae5cdd93eb2941ba123a2
Wenceslao Negrete <vampay@digitaldealers.mx>
2018-08-23 22:11:51 -0500

    Nuevas env vars en config

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
   client: 'mysql',
   connection: {
-    host: '127.0.0.1',
-    user: 'admin',
-    password: '12345',
-    database: 'laboratoria'
+    host: process.env.DB_HOST || '127.0.0.1',
+    user: process.env.DB_USER || 'admin',
+    password: process.env.DB_PASS || '12345',
+    database: process.env.DB_NAME || 'laboratoria'
   }
 });
 
